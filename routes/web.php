dc7765e7c183dad2bd7dabc64f103cad11744977
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-10-13 03:48:50 +0300

    implemented about page backend and frontend, but still have problem in images to be displayed

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,56 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home\AboutController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-   // return view('welcome');
-   return view('frontend.index');
+    return view('frontend.index');
 });
 
+ 
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'Index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'ContactMethod')->name('cotact.page');
+});
+
+
+ // Admin All Route 
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+
+    Route::get('/change/password', 'ChangePassword')->name('change.password');
+    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+     
+});
+
+
+ // Home Slide All Route 
+Route::controller(HomeSliderController::class)->group(function () {
+    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+   Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+     
+});
+
+ // About Page All Route 
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about/page', 'AboutPage')->name('about.page');
+    Route::post('/update/about', 'UpdateAbout')->name('update.about');
+    Route::get('/about', 'HomeAbout')->name('home.about');
+   
+     
+});
+
+
+
+
+ 
 
 
 
@@ -28,38 +58,8 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('admin.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-
-
-
-
-
-
-
-//Admin All route
-Route::controller(AdminController::class)->group(function(){
-    Route::get('/admin/logout','destroy')->name('admin.logout');
-    Route::get('/admin/profile','profile')->name('admin.profile');
-    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
-    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
-    Route::get('/change/password', 'ChangePassword')->name('change.password');
-    Route::post('/update/password', 'UpdatePassword')->name('update.password');
-});
-
- // Home Slide All Route 
- Route::controller(HomeSliderController::class)->group(function () {
-    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
-   Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
-     
-});
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
